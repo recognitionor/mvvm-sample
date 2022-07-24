4e35c86f432c0a6b624000ce0efb04d17b985a29
junghee <junghee@neowiz.com>
2022-07-24 23:57:06 +0900

    error resolve

--- app/src/main/java/com/recognitionor/mvvm_sample/data/local/dao/DBTestDao.kt
+++ app/src/main/java/com/recognitionor/mvvm_sample/data/local/dao/DBTestDao.kt
@@ -1,7 +1,6 @@
 package com.recognitionor.mvvm_sample.data.local.dao
 
-import androidx.room.*
-import com.recognitionor.mvvm_sample.data.local.entity.DBTest
+import androidx.room.Dao
 
 @Dao
 interface DBTestDao {
